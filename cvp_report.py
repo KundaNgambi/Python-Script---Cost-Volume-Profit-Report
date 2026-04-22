ad2b078b1d9b9d4c7c8d9cbb7ffabed5e2cd843f
kunda_ngambi <142142359+KundaNgambi@users.noreply.github.com>
2026-04-22 22:28:21 +0200

    Added the following to the data dic
    - row['Contribution per Unit(ZMW)']
    - row['Contribution Ratio']
    - row['Break Even(Units)']
    - row['Break Even(Revenue)']
    - row['Margin of Safety(Units)']
    - row['Margin of Safety(%)']
    - row['Units to Achieve Target']

--- cvp_report.py
+++ cvp_report.py
@@ -1,6 +1,8 @@
 import csv
+import math
 
 REPORT_FILE = 'sample_data.csv'
+target_profit = float(input('Enter target profit: '))
 
 data = []
 # utf-8-sig strips the BOM Excel prepends to UTF-8 CSVs (otherwise it appears in the first column name).
@@ -13,5 +15,17 @@ with open(REPORT_FILE, encoding='utf-8-sig', newline = '') as csvfile:
         row['Units'] = int(float(row['Units']))
 
         data.append(row)
+
+
+
+for row in data:
+    row['Contribution per Unit(ZMW)'] = row['Selling Price(ZMW)'] - row['Variable Cost per Unit(ZMW)']
+    row['Contribution Ratio'] = (row['Selling Price(ZMW)'] - row['Variable Cost per Unit(ZMW)'])/row['Selling Price(ZMW)']
+    row['Break Even(Units)'] = math.ceil(row['Fixed Costs Attributable(ZMW)']/ row['Contribution per Unit(ZMW)'])
+    row['Break Even(Revenue)'] = row['Fixed Costs Attributable(ZMW)'] / row['Contribution Ratio']
+    row['Margin of Safety(Units)'] = math.ceil(row['Units'] - row['Break Even(Units)'])
+    row['Margin of Safety(%)'] = round(((row['Units'] - row['Break Even(Units)'])/row['Units'])*100,2)
+    row['Units to Achieve Target'] = math.ceil((row['Fixed Costs Attributable(ZMW)'] + target_profit)/row['Contribution per Unit(ZMW)'])
+
 
 print(data)
